bc621ef48d37a76b51b50a411d9802e8a96c906c
Mario <mario.lucki@dings.solutions>
2020-04-18 20:04:30 +0200

    Add economy scraper to index js

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const puppeteer = require("puppeteer");
+
+app.get("/economy", async function (req, res) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://tradingeconomics.com/");
+  await page.click("#ctl00_ContentPlaceHolder1_ctl04_LinkButton1");
+  await page.waitFor(1000);
+
+  const extractedEconomicData = await page.evaluate(() => {
+    const [, ...economicData] = [
+      ...document.getElementById("ctl00_ContentPlaceHolder1_ctl04_GridView1")
+        .childNodes[1]["children"],
+    ].map((x) => [...x.cells].map((p) => p.textContent.replace(/\s/g, "")));
+    return economicData;
+  });
+
+  const economicDataParsed = extractedEconomicData.map(
+    ([
+      country,
+      gdp,
+      gdpYoy,
+      gdpQoq,
+      interestRate,
+      inflationRate,
+      joblessRate,
+      govBudget,
+      debtGDP,
+      currentAccount,
+      population,
+    ]) => ({
+      country,
+      gdp,
+      gdpYoy,
+      gdpQoq,
+      interestRate,
+      inflationRate,
+      joblessRate,
+      govBudget,
+      debtGDP,
+      currentAccount,
+      population,
+    })
+  );
+
+  await browser.close();
+
+  await res.send(economicDataParsed);
+});
+
+const port = process.env.PORT || 3051;
+app.listen(port, function () {});
